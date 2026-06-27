002f84efa91c96233c6ffcc0c4ae900de6f76635
Martin Mahonga <martinmahonga@gmail.com>
2026-06-27 08:44:29 +0100

    feat: add trustProxies middleware configuration for improved proxy handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*'
+        );
+        
         $middleware->api(prepend: [
             HandleCors::class,
         ]);
